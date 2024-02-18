bb338c9365bc2eec288e84088cd2e9a13a8a0038
tegar-adty <147395970+tegar-adty@users.noreply.github.com>
2024-02-19 00:55:49 +0700

    Add files via upload

---
+++ Hitungumur.py
@@ -0,0 +1,6 @@
+tahunlahir = float(input("masukan tahun lahir kamu:"))
+tahunsekarang = float(input("masukan tahun sekarang:"))
+
+umur = tahunsekarang-tahunlahir
+
+print("umur anda adalah: %.0f tahun" % umur)
---
+++ suhu.py
@@ -0,0 +1,9 @@
+reamur = float(input("masukan suhu dalam reamur:"))
+
+celcius = (5/4) * reamur
+fahrenheit = (9/4) * reamur
+kelvin = (5/4) * reamur 
+
+print("konversi ke celcius: %.2f" % celcius)
+print("konversi ke fahrenheit: %.2f" % fahrenheit)
+print("konversi ke kelvin: %.2f" % kelvin)
